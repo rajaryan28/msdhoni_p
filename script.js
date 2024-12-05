d3f07cb5fd5629fb7da737bd645321350e580b2c
rajaryan2816 <rajaryan2816@gmail.com>
2024-12-05 19:23:05 +0530

    Added Cursor effect

--- script.js
+++ script.js
@@ -124,7 +124,27 @@ updateCounter();
 
 
 
-//image slidder
+// coursor effect
+
+const cursorDot = document.querySelector('#cursor-dot')
+const cursorOutline = document.querySelector('#cursor-outline')
+
+window.addEventListener('mousemove',function(e){
+    const posX = e.clientX
+    const posY = e.clientY
+
+    cursorDot.style.left=`${posX}px`
+    cursorDot.style.top=`${posY}px`
+
+    // cursorOutline.style.left = `${posX}px`
+    // cursorOutline.style.top = `${posY}px`
+
+    cursorOutline.animate({
+        left:`${posX}px`,
+        top:`${posY}px`
+    },{duration:500,fill:'forwards'});
+
+})
 
 
     
